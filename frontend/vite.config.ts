0332bb2ea28765bad0858fd436be5e9e7548e7e3
Vactor0911 <vactor0911@gmail.com>
2025-06-27 03:00:56 +0900

    GitHub Pages 배포 환경 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+  ],
+  base: "/project-mw",
+  server: {
+    port: 3000,
+    allowedHosts: ["0.tcp.jp.ngrok.io"],
+  },
+});

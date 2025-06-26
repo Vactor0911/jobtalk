f580a06da1f4fea4952af521a54dcfeebc5fbe99
Vactor0911 <vactor0911@gmail.com>
2025-06-27 04:15:23 +0900

    백엔드 index.ts 파일 수정

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
+import dotenv from "dotenv";
 import express from "express";
 
-const PORT = 3000; // 서버가 실행될 포트 번호
+dotenv.config(); // .env 환경 변수 로드
+
+const PORT = Number(process.env.SERVER_PORT); // 서버가 실행될 포트 번호
 
 const app = express();
-
 app.use(express.json()); // JSON 요청을 처리하기 위한 미들웨어
 
 // 기본 라우트 설정

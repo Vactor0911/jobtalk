8948c772079cdfa8f26651634055cc6c9226187a
Vactor0911 <vactor0911@gmail.com>
2025-06-27 02:16:01 +0900

    백엔드 프로젝트 파일 생성

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const PORT = 3000; // 서버가 실행될 포트 번호
+
+const app = express();
+
+app.use(express.json()); // JSON 요청을 처리하기 위한 미들웨어
+
+// 기본 라우트 설정
+app.get("/", (req, res) => {
+  res.send("Project MW Web Server!");
+});
+
+// 서버 시작
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
+});

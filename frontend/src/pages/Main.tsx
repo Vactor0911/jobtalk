8ed28cbbdb6dec166b64daf30efa1d96ace0e257
Vactor0911 <vactor0911@gmail.com>
2025-07-02 02:00:02 +0900

    메인 페이지 추가

---
+++ frontend/src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import { Container, Stack } from "@mui/material";
+
+const Main = () => {
+  return (
+    <Container maxWidth="xl">
+      <Stack></Stack>
+    </Container>
+  );
+};
+
+export default Main;

73c271125eafde4975fa13be26c0ecf8c9a12d0d
Vactor0911 <vactor0911@gmail.com>
2025-07-02 14:37:16 +0900

    패널 너비 조절 기능 구현

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,48 +1,84 @@
 import { Box, Paper, Stack } from "@mui/material";
 import TitledContainer from "../components/TitledContainer";
+import { Panel, PanelGroup } from "react-resizable-panels";
 import Test from "../components/Test";
+import StyledPanelResizeHandle from "../components/StyledPanelResizeHandle";
+import React, { useMemo, useState } from "react";
 
 const Main = () => {
+  // 세부사항 패널
+  const detailsPanel = useMemo(() => {
+    return (
+      <Panel
+        defaultSize={25}
+        minSize={20}
+        css={{
+          height: "100%",
+        }}
+      >
+        <TitledContainer title="세부사항"></TitledContainer>
+      </Panel>
+    );
+  }, []);
+
+  // 로드맵 패널
+  const roadmapPanel = useMemo(() => {
+    return (
+      <Panel
+        minSize={20}
+        css={{
+          height: "100%",
+        }}
+      >
+        <TitledContainer title="로드맵">
+          <Test />
+        </TitledContainer>
+      </Panel>
+    );
+  }, []);
+
+  // 챗봇 패널
+  const chatbotPanel = useMemo(() => {
+    return (
+      <Panel
+        defaultSize={25}
+        minSize={20}
+        css={{
+          height: "100%",
+        }}
+      >
+        <TitledContainer title="챗봇">
+          <Stack height="100%">
+            {/* 공간 차지용 */}
+            <Box flex={1} />
+
+            {/* 채팅란 */}
+            <Paper
+              variant="outlined"
+              sx={{ height: "100px", borderRadius: 4 }}
+            ></Paper>
+          </Stack>
+        </TitledContainer>
+      </Panel>
+    );
+  }, []);
+
+  const [panels, setPanels] = useState([
+    detailsPanel,
+    roadmapPanel,
+    chatbotPanel,
+  ]);
+
   return (
-    <Stack padding={2} direction="row" height="calc(100vh - 64px)" gap={2}>
-      {/* 세부사항 */}
-      <TitledContainer
-        title="세부사항"
-        sx={{
-          flex: 1,
-          maxWidth: 450,
-        }}
-      ></TitledContainer>
-
-      {/* 로드맵 */}
-      <TitledContainer
-        title="로드맵"
-        sx={{
-          flex: 2,
-        }}
-      >
-        <Test />
-      </TitledContainer>
-
-      {/* 챗봇 */}
-      <TitledContainer
-        title="챗봇"
-        sx={{
-          flex: 1,
-          maxWidth: 450,
-        }}
-      >
-        <Stack height="100%">
-          {/* 공간 차지용 */}
-          <Box flex={1} />
-
-          {/* 채팅란 */}
-          <Paper
-            variant="outlined"
-            sx={{ height: "100px", borderRadius: 4 }}
-          ></Paper>
-        </Stack>
-      </TitledContainer>
+    <Stack padding={2} direction="row" height="calc(100vh - 64px)">
+      <PanelGroup direction="horizontal">
+        {panels.map((panel, index) => (
+          <React.Fragment key={index}>
+            {panel}
+            {index < panels.length - 1 && <StyledPanelResizeHandle />}
+          </React.Fragment>
+        ))}
+      </PanelGroup>
     </Stack>
   );
 };

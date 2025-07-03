c12a66506559eaa8f8789b08e39e5b1ceac39d92
L-Hits <leechan753@gmail.com>
2025-07-04 00:31:58 +0900

    토큰 리프레셔를 상단에 두어 토큰 재발급 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { theme } from "./utils/theme";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { Login, Main, Profile, Register, Workspace } from "./pages";
 import Header from "./components/Header";
+import TokenRefresher from "./components/TokenRefresher";
 
 const App = () => {
   return (
@@ -10,15 +11,17 @@ const App = () => {
       <CssBaseline />
 
       <BrowserRouter basename="/jobtalk">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/roadmap/:roadmapId" element={<Workspace />} />
-          <Route path="/*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <TokenRefresher>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/roadmap/:roadmapId" element={<Workspace />} />
+            <Route path="/*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </TokenRefresher>
       </BrowserRouter>
     </ThemeProvider>
   );

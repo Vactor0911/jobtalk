1ad4d5ac387483cc66a83fcd6409cc3eb7949d93
Vactor0911 <vactor0911@gmail.com>
2025-07-03 21:00:47 +0900

    워크스페이스 페이지 리팩토링

--- frontend/src/pages/Workspace.tsx
+++ frontend/src/pages/Workspace.tsx
@@ -3,77 +3,54 @@ import TitledContainer from "../components/TitledContainer";
 import { Panel, PanelGroup } from "react-resizable-panels";
 import Test from "../components/Test";
 import StyledPanelResizeHandle from "../components/StyledPanelResizeHandle";
-import React, { useMemo, useState } from "react";
-import JobSearchChat from "../components/JobSearchChat";
 import ChatTest from "../components/ChatTest";
 
 const Workspace = () => {
-  // 세부사항 패널
-  const detailsPanel = useMemo(() => {
-    return (
-      <Panel
-        defaultSize={25}
-        minSize={20}
-        css={{
-          height: "100%",
-        }}
-      >
-        <TitledContainer title="세부사항"></TitledContainer>
-      </Panel>
-    );
-  }, []);
-
-  // 로드맵 패널
-  const roadmapPanel = useMemo(() => {
-    return (
-      <Panel
-        minSize={20}
-        css={{
-          height: "100%",
-        }}
-      >
-        <TitledContainer title="로드맵">
-          <Test />
-        </TitledContainer>
-      </Panel>
-    );
-  }, []);
-
-  // 챗봇 패널
-  const chatbotPanel = useMemo(() => {
-    return (
-      <Panel
-        defaultSize={25}
-        minSize={20}
-        css={{
-          height: "100%",
-        }}
-      >
-        <TitledContainer
-          title="챗봇"
-        >
-          {/* <JobSearchChat /> */}
-          <ChatTest />
-        </TitledContainer>
-      </Panel>
-    );
-  }, []);
-
-  const [panels, setPanels] = useState([
-    detailsPanel,
-    roadmapPanel,
-    chatbotPanel,
-  ]);
-
   return (
     <Stack padding={2} direction="row" height="calc(100vh - 64px)">
       <PanelGroup direction="horizontal">
-        {panels.map((panel, index) => (
-          <React.Fragment key={index}>
-            {panel}
-            {index < panels.length - 1 && <StyledPanelResizeHandle />}
-          </React.Fragment>
-        ))}
+        {/* 세부사항 패널 */}
+        <Panel
+          defaultSize={25}
+          minSize={20}
+          css={{
+            height: "100%",
+          }}
+        >
+          <TitledContainer title="세부사항"></TitledContainer>
+        </Panel>
+
+        {/* 구분선 */}
+        <StyledPanelResizeHandle />
+
+        {/* 로드맵 패널 */}
+        <Panel
+          minSize={20}
+          css={{
+            height: "100%",
+          }}
+        >
+          <TitledContainer title="로드맵">
+            <Test />
+          </TitledContainer>
+        </Panel>
+
+        {/* 구분선 */}
+        <StyledPanelResizeHandle />
+
+        {/* 챗봇 패널 */}
+        <Panel
+          defaultSize={25}
+          minSize={20}
+          css={{
+            height: "100%",
+          }}
+        >
+          <TitledContainer title="챗봇">
+            {/* <JobSearchChat /> */}
+            <ChatTest />
+          </TitledContainer>
+        </Panel>
       </PanelGroup>
     </Stack>
   );

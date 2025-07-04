9297d86d7c39756982ff3ef218641498ab6188d6
L-Hits <leechan753@gmail.com>
2025-07-04 23:29:34 +0900

    모든 자격증 종목 조회 API 구현

--- backend/src/routes/qualificationRoute.ts
+++ backend/src/routes/qualificationRoute.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { limiter } from "../utils";
 import {
+  getAllQualifications,
   searchQualificationsFromDB,
 } from "../controllers/qualificationController";
 
@@ -8,5 +9,8 @@ const qualificationRoute = express.Router();
 
 // 자격증 검색 API
 qualificationRoute.get("/search", limiter, searchQualificationsFromDB);
+
+// 모든 자격증 종목 조회 API
+qualificationRoute.get("/all", limiter, getAllQualifications);
 
 export default qualificationRoute;

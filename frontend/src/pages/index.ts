2d65c20d4425d5b80b0bf4e815fd5a3ffc170e44
Vactor0911 <vactor0911@gmail.com>
2025-07-04 19:35:42 +0900

    내 워크스페이스 페이지 구현

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -3,5 +3,6 @@ import Login from "./Login";
 import Register from "./Register";
 import Workspace from "./Workspace";
 import Profile from "./Profile";
+import MyWorkspace from "./MyWorkspace";
 
-export { Main, Login, Register, Workspace, Profile };
+export { Main, Login, Register, Workspace, Profile, MyWorkspace };

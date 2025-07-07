bb6c0c501a968fdcf4636e794c801802c2da2952
Vactor0911 <vactor0911@gmail.com>
2025-07-08 03:15:21 +0900

    로드맵 뷰어 구현

---
+++ frontend/src/components/workspace/RoadMapView.tsx
@@ -0,0 +1,58 @@
+import { Stack } from "@mui/material";
+import { Panel, PanelGroup } from "react-resizable-panels";
+import TitledContainer from "../TitledContainer";
+import StyledPanelResizeHandle from "../StyledPanelResizeHandle";
+import ChatTest from "../ChatTest";
+
+const RoadMapView = () => {
+  return (
+    <Stack padding={2} direction="row" height="calc(100vh - 64px)">
+      <PanelGroup direction="horizontal">
+        {/* 세부사항 패널 */}
+        <Panel
+          defaultSize={25}
+          minSize={20}
+          css={{
+            height: "100%",
+          }}
+        >
+          <TitledContainer title="세부사항"></TitledContainer>
+        </Panel>
+
+        {/* 구분선 */}
+        <StyledPanelResizeHandle />
+
+        {/* 로드맵 패널 */}
+        <Panel
+          minSize={20}
+          css={{
+            height: "100%",
+          }}
+        >
+          <TitledContainer title="로드맵">
+            {/* TODO: 로드맵 뷰어 구현 */}
+          </TitledContainer>
+        </Panel>
+
+        {/* 구분선 */}
+        <StyledPanelResizeHandle />
+
+        {/* 챗봇 패널 */}
+        <Panel
+          defaultSize={25}
+          minSize={20}
+          css={{
+            height: "100%",
+          }}
+        >
+          <TitledContainer title="챗봇">
+            {/* <JobSearchChat /> */}
+            <ChatTest />
+          </TitledContainer>
+        </Panel>
+      </PanelGroup>
+    </Stack>
+  );
+};
+
+export default RoadMapView;

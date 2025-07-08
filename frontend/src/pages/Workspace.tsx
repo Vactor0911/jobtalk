1b3a4742e77416556fbec89b63a67b18a76e91f4
Vactor0911 <vactor0911@gmail.com>
2025-07-08 15:36:40 +0900

    워크스페이스 페이지에 스테퍼 적용

--- frontend/src/pages/Workspace.tsx
+++ frontend/src/pages/Workspace.tsx
@@ -1,16 +1,28 @@
 import { Button, Container, Stack, Typography } from "@mui/material";
-import RoadMapView from "../components/workspace/RoadMapView";
 import WorkspaceStepper from "../components/workspace/WorkspaceStepper";
 import { useState } from "react";
 import InterestsView from "../components/workspace/InterestsView";
+import ChatbotView from "../components/workspace/ChatbotView";
 
 const Workspace = () => {
   const [step, setStep] = useState(1);
 
   return (
     <Container maxWidth="lg">
-      <Stack minHeight="calc(100vh - 64px)" paddingY={4} paddingBottom={10} gap={4}>
-        <WorkspaceStepper activeStep={step} />
+      <Stack
+        minHeight="calc(100vh - 64px)"
+        paddingY={4}
+        paddingBottom={10}
+        gap={4}
+      >
+        {/* 스테퍼 */}
+        {step <= 5 && <WorkspaceStepper activeStep={step} />}
+
+        {/* 관심 분야 선택 */}
+        {step === 1 && <InterestsView />}
+
+        {/* 챗봇 질문 */}
+        {step === 3 && <ChatbotView />}
 
         {/* 스텝 테스트용 버튼 */}
         <Stack direction="row" gap={2} justifyContent="center">
@@ -34,8 +46,6 @@ const Workspace = () => {
             다음
           </Button>
         </Stack>
-
-        <InterestsView />
       </Stack>
     </Container>
   );

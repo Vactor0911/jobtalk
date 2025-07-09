03a4b5069bd3da95b4be5a8fe91e4d27d9f1656c
Vactor0911 <vactor0911@gmail.com>
2025-07-10 04:00:07 +0900

    framer-motion 요소 리팩토링

--- frontend/src/components/framer-motion/MotionWrapper.tsx
+++ frontend/src/components/framer-motion/MotionWrapper.tsx
@@ -1,30 +1,33 @@
-import { motion } from "framer-motion";
+import { motion, type HTMLMotionProps } from "framer-motion";
 import type { ReactNode } from "react";
 
-const MotionWrapper = ({
-  children,
-  direction,
-  initialOffset,
-}: {
+interface MotionWrapperProps extends HTMLMotionProps<"div"> {
   children: ReactNode;
-  direction?: "x" | "y";
-  initialOffset?: number;
-}) => (
-  <motion.div
-    variants={{
-      hidden: { [direction || "y"]: [initialOffset || -50], opacity: 0 },
-      show: {
-        [direction || "y"]: 0,
-        opacity: 1,
-      },
-    }}
-    transition={{
-      duration: 1,
-      ease: "easeInOut",
-    }}
-  >
-    {children}
-  </motion.div>
-);
+  direction?: "x" | "y"; // 방향 설정, 기본값은 'y'
+  initialOffset?: number; // 초기 오프셋, 기본값은 -50
+}
+
+const MotionWrapper = (props: MotionWrapperProps) => {
+  const { children, direction, initialOffset, ...others } = props;
+
+  return (
+    <motion.div
+      variants={{
+        hidden: { [direction || "y"]: [initialOffset || -50], opacity: 0 },
+        show: {
+          [direction || "y"]: 0,
+          opacity: 1,
+        },
+      }}
+      transition={{
+        duration: 1,
+        ease: "easeInOut",
+      }}
+      {...others}
+    >
+      {children}
+    </motion.div>
+  );
+};
 
 export default MotionWrapper;

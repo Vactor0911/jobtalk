5409fd93177afccb8c83061ecb6f9636df283449
L-HITS <leechan753@gmail.com>
2025-09-21 23:39:35 +0900

    모든 api에 csrfProtection 제거

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { csrfProtection, limiter, refreshTokenLimiter } from "../utils";
+import { limiter, refreshTokenLimiter } from "../utils";
 import {
   deleteAccount,
   getUserInfo,
@@ -23,10 +23,10 @@ const authRoute = express.Router();
 authRoute.post("/register", register);
 
 // 로그인
-authRoute.post("/login", csrfProtection, login);
+authRoute.post("/login", login);
 
 // 로그아웃
-authRoute.post("/logout", csrfProtection, logout);
+authRoute.post("/logout", logout);
 
 // 이메일 인증 요청
 authRoute.post("/sendVerifyEmail", sendVerifyEmail);
@@ -35,27 +35,27 @@ authRoute.post("/sendVerifyEmail", sendVerifyEmail);
 authRoute.post("/verifyEmailCode", verifyEmailCode);
 
 // 엑세스 토큰 재발급
-authRoute.post("/token/refresh", csrfProtection, refreshTokenLimiter, refreshToken);
+authRoute.post("/token/refresh", refreshTokenLimiter, refreshToken);
 
 // 사용자 정보 조회
-authRoute.get("/me", csrfProtection, limiter, authenticateToken, getUserInfo);
+authRoute.get("/me", limiter, authenticateToken, getUserInfo);
 
 // 닉네임 변경
-authRoute.patch("/me/nickname", csrfProtection, limiter, authenticateToken,  updateNickname);
+authRoute.patch("/me/nickname", limiter, authenticateToken,  updateNickname);
 
 // 비밀번호 변경
-authRoute.patch("/me/password", csrfProtection, limiter, authenticateToken, updatePassword);
+authRoute.patch("/me/password", limiter, authenticateToken, updatePassword);
 
 // 계정 탈퇴
-authRoute.post("/me/delete", csrfProtection, limiter, authenticateToken, deleteAccount);
+authRoute.post("/me/delete", limiter, authenticateToken, deleteAccount);
 
 // 프로필 이미지 업로드
-authRoute.post("/me/profile-image", csrfProtection, limiter, authenticateToken, uploadProfileImage);
+authRoute.post("/me/profile-image", limiter, authenticateToken, uploadProfileImage);
 
 // 자격증 정보 업데이트
-authRoute.patch("/me/certificates", csrfProtection, limiter, authenticateToken, updateUserCertificates);
+authRoute.patch("/me/certificates", limiter, authenticateToken, updateUserCertificates);
 
 // 비밀번호 찾기(재설정)
-authRoute.post("/resetPassword", csrfProtection, resetPassword);
+authRoute.post("/resetPassword", resetPassword);
 
 export default authRoute;
